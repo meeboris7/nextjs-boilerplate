c0714e09d101e07e208c4535afc21fd552a251a7
meeboris7 <meet.borisagar@publicissapient.com>
2025-07-07 05:26:35 +0000

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+
+  experimental: {
+   
+    serverComponentsExternalPackages: ['pdf-parse'],
+  },
+
+  
 };
 
 export default nextConfig;
